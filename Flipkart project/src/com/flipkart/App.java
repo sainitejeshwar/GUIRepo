253cbf360657be1783351ab411e0805bf3acaeb4
tejas <tejeshwar.saini7@gmail.com>
2020-05-30 16:41:52 +0530

    Added employee Class

--- Flipkart project/src/com/flipkart/App.java
+++ Flipkart project/src/com/flipkart/App.java
@@ -1,10 +1,19 @@
 package com.flipkart;
 
+import com.flipkart.boom.Employee;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello There !!");
+		System.out.println("new");
+		
+		Employee e1 = new Employee();
+		e1.setId(101);
+		e1.setName("Tejeshwar Saini");
+		System.out.println(e1.getId());
+		System.out.println(e1.getName());
 
 	}
 
